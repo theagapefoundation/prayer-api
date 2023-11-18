740925f8a3d6bac4c28840601ee18149ab7b3c7e
Jay Cho <jaycho1214@gmail.com>
2023-11-18 20:45:03 +0900

    add guard for fetching timezone from client

---
+++ src/timezone.guard.ts
@@ -0,0 +1,8 @@
+import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+
+export const Timezone = createParamDecorator((data, ctx: ExecutionContext) => {
+  const request = ctx.switchToHttp().getRequest();
+  const offset =
+    parseInt(request.headers['x-timezone-offset'], 10) || undefined;
+  return offset;
+});
