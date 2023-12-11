31f17382d4ebf2e8fb033c04e3279412ed55917c
Jay Cho <jaycho1214@gmail.com>
2023-12-11 16:03:14 +0900

    fix unused import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { GroupsModule } from './groups/groups.module';
